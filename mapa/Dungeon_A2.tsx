9338fb33cfaf6718476965ac5114d9cafee6669e
AzazelsLamb <you@example.com>
2018-10-28 02:47:52 -0300

    elimino archivos innecesarios

--- mapa/Dungeon_A2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="Dungeon_A2" tilewidth="24" tileheight="24" tilecount="768" columns="32">
- <image source="Dungeon_A2.png" width="768" height="576"/>
-</tileset>
