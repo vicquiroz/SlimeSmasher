68a27a1539109fabc9a3eb24256aeb002060fcde
Katechi <katerinne.leiva@gmail.com>
2018-10-31 12:17:14 -0300

    movimiento en unity
    Game Controller

---
+++ SlimeUnity/Assets/Attack.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+
+public class Attack : MonoBehaviour {
+
+
+
+public float speed = 100;
+
+	
+// Use this for initialization
+	
+void Start () {
+	
+	
+}
+	
+	
+// Update is called once per frame
+	
+void Update () {
+		
+transform.Translate (-Vector2.up * Time.deltaTime * speed);
+	}
+}
---
+++ SlimeUnity/Assets/CharacterController2D.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour { 
+  
+     public float speed = 75;
+ 
+          Rigidbody2D rb;
+          Animator anim;
+	// Use this for initialization
+	void Start () {
+          rb=GetComponent < Rigidbody2D>();
+anim=GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+ Vector2 movement=new Vector2(Input.GetAxis ("Horizontal"),Input.GetAxis ("Vertical"));
+
+rb.AddForce (movement*Time.deltaTime*speed);
+
+  if (Input.GetAxis("Horizontal")>0) {
+     anim.SetTrigger ("Right");
+     if (Input.GetButtonDown ("Fire1")) {
+   
+     anim.SetTrigger("Attack R");
+     
+    }
+     
+		
+	}
+if (Input.GetAxis("Horizontal")<0) {
+     anim.SetTrigger ("Left");
+      if (Input.GetButtonDown ("Fire1")) {
+   
+     anim.SetTrigger("Attack L");
+     
+    }
+		
+	}
+if (Input.GetAxis("Vertical")>0) {
+     anim.SetTrigger ("Up");
+   if (Input.GetButtonDown ("Fire1")) {
+   
+     anim.SetTrigger("Attack U");
+     
+    }
+    
+
+		
+	}
+if (Input.GetAxis("Vertical")<0) {
+     anim.SetTrigger ("Down");
+   if (Input.GetButtonDown ("Fire1")) {
+   
+     anim.SetTrigger("AttackD");
+     
+    }
+    }
+
+   
+   
+   
+      
+}
+}
