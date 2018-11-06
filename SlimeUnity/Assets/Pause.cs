07eecfdd9eb66bae5eafa6258e91cf2f86fe188f
Katechi <katerinne.leiva@gmail.com>
2018-11-06 10:55:58 -0300

    Se añade desarrollo de UI específicamente menú  y sistema de pausa.

---
+++ SlimeUnity/Assets/Pause.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+        bool active;
+        Canvas canvas;
+
+	// Use this for initialization
+	void Start () {
+          canvas = GetComponent<Canvas>();
+          canvas.enabled= false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            if (Input.GetKeyDown("space")){
+             active =!active;
+             canvas.enabled =active;
+             Time.timeScale = (active) ? 0:1f;
+     }
+		
+	}
+}
