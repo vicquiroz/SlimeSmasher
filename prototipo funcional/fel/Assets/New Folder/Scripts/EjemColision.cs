e6a7e40a4bae1ce765dd8dc4fce42e1b256db185
Patricio <ptorresmanque@gmail.com>
2018-11-14 22:20:59 -0300

    ejemplo de colision
    ahi le puse un script que detecta la colision con el slime y manda un mensaje a consola

---
+++ prototipo funcional/fel/Assets/New Folder/Scripts/EjemColision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjemColision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "slime")
+        {
+            restarPuntos();
+        }
+    }
+
+    void restarPuntos()
+    {
+        Debug.Log("En esta funcion deberian hacer la resta de puntos, eliminar el slime, etc, etc");
+    }
+}
