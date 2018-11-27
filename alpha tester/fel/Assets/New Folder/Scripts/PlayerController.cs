c8df49cde1425ce4667ab42a97b1301d1476053a
AzazelsLamb <you@example.com>
2018-11-27 11:47:07 -0300

    Prototippo listo para cambios (spawn de diferentes colores)

--- alpha tester/fel/Assets/New Folder/Scripts/PlayerController.cs
+++ alpha tester/fel/Assets/New Folder/Scripts/PlayerController.cs
@@ -25,18 +25,18 @@ public class PlayerController : MonoBehaviour {
 
 		if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
 		{
-			transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
-			//myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed,myRigidbody.velocity.y);
+			//transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+			myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed,myRigidbody.velocity.y);
 			PlayerMoving = true;
 			lastMove = new Vector3(Input.GetAxisRaw("Horizontal"), 0f,0f);	
 		}	
 
 		if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
 		{
-			transform.Translate (new Vector3(0f, 0f,  Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime));
-			//myRigidbody.velocity = new Vector2(myRigidbody.velocity.x,Input.GetAxisRaw("Vertical") * moveSpeed);
+			//transform.Translate (new Vector3(0f,   Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime),0f);
+			myRigidbody.velocity = new Vector2(myRigidbody.velocity.x,Input.GetAxisRaw("Vertical") * moveSpeed);
 			PlayerMoving = true;
-			lastMove = new Vector3(0f,0f, Input.GetAxisRaw("Vertical"));
+			lastMove = new Vector3(0f, Input.GetAxisRaw("Vertical"),0f);
 		}
 		/*if(Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f){
 
@@ -51,7 +51,7 @@ public class PlayerController : MonoBehaviour {
 		anim.SetFloat ("MoveY", Input.GetAxisRaw("Vertical"));
 		anim.SetBool("PlayerMoving", PlayerMoving);
 		anim.SetFloat("LastMoveX",lastMove.x);
-		anim.SetFloat("LastMoveY",lastMove.z);
+		anim.SetFloat("LastMoveY",lastMove.y);
 
 	}
 }
